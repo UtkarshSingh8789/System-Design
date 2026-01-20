48e2d386652ba2512b456aaad49477f14c9fb0db
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2026-01-20 23:23:37 +0530

    push Method example;

--- LLD/DesignPattern/ObservePattern/weather.cpp
+++ LLD/DesignPattern/ObservePattern/weather.cpp
@@ -24,12 +24,62 @@
 using namespace std;
 #define ll long long
 // Push-method;
-class WeatherObserver{
-    public:
-    
+class WeatherData{
+    float temperature;
+    float humidity;
+public:
+    WeatherData(float temp,float humidity){
+        this->temperature=temp;
+        this->humidity=humidity;
+    }
+    float getTemperature(){
+        return temperature;
+    }
+    float getHumidity(){
+        return humidity;
+    }
 };
-class WeatherObservable{
-    
+class WeatherObserver{
+public:
+    virtual void update(WeatherData* d)=0;
+};
+class WeatherObservable{ // this is an interface;
+public:
+    virtual void addObserver(WeatherObserver* obs)=0;
+    virtual void removeObserver(WeatherObserver* obs)=0;
+    virtual void notify()=0;
+    virtual void setWeatherReadings(float temp,float humidity)=0;
+};
+class ConcreteObservable : public WeatherObservable{
+    vector<WeatherObserver*> subscribers;
+    WeatherData* data;
+public:
+    void addObserver(WeatherObserver* obs) override {
+        subscribers.push_back(obs);
+    }
+    void removeObserver(WeatherObserver* obs) override {
+        for(auto it=subscribers.begin();it!=subscribers.end();it++){
+            if(*it==obs){
+                subscribers.erase(it);
+                break;
+            }
+        }
+    }
+    void notify() override {
+        for(auto it:subscribers){
+            (it)->update(data);
+        }
+    }
+    void setWeatherReadings(float temp,float humidity){
+        data=new WeatherData(temp,humidity);
+        notify();
+    }
+};
+class concreteObserver : public WeatherObserver{
+public:
+    void update(WeatherData* d) override {
+        // now use a business logic to use the data;
+    }
 };
 int main(){
     ios::sync_with_stdio(false);
