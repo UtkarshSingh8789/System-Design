8ed482e06eb7ad2021ca8988fa323bd1d8d87f8b
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2026-01-22 12:44:15 +0530

    Factory Design Pattern

--- LLD/DesignPattern/FactoryPattern/NotificationManager.cpp
+++ LLD/DesignPattern/FactoryPattern/NotificationManager.cpp
@@ -23,3 +23,51 @@
 #include <unordered_set>
 using namespace std;
 #define ll long long
+class Notification {
+public:
+    virtual void send(string msg)=0;
+    virtual ~Notification() {}
+};
+class EmailNotification : public Notification {
+public:
+    void send(string msg) override {
+        cout<<"Email Sent: "<<msg<<endl;
+    }
+};
+class SMSNotification : public Notification {
+public:
+    void send(string msg) override {
+        cout<<"SMS Sent: "<<msg<<endl;
+    }
+};
+class PushNotification : public Notification {
+public:
+    void send(string msg) override {
+        cout<<"Push Message Sent: "<<msg<<endl;
+    }
+};
+class NotificationFactory{
+public:
+    Notification* createMessage(string type){
+        if(type=="Email"){
+            return new EmailNotification();
+        }
+        else if(type=="SMS"){
+            return new SMSNotification();
+        }
+        else if(type=="push"){
+            return new PushNotification();
+        }
+        else return NULL;
+    }
+};
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // client side code;   
+    NotificationFactory Factory;
+    Notification* n1=Factory.createMessage("Email");
+    Notification* n2=Factory.createMessage("SMS");  
+    n1->send("Welcome Utkarsh");
+    return 0;                                                                                      
+}
