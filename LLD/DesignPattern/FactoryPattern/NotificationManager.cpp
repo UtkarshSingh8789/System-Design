fcc15ae0eb3e481a53b334ba9da248f9e25c18f6
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2026-01-22 12:04:46 +0530

    start

---
+++ LLD/DesignPattern/FactoryPattern/NotificationManager.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+#define ll long long
